ebfa2f3520728fe953225f279be284493f23fd2f
g3ryll <121690640+g3ryll@users.noreply.github.com>
2023-02-01 00:34:14 +0000

    Update snake_game.py

--- snake_game.py
+++ snake_game.py
@@ -1,127 +1,139 @@
-import pygame
-import time
-import random
- 
+import pygame,sys,random
+from pygame.math import Vector2
+
+# Colors (R, G, B)
+black = (0, 0, 0)              #black
+cinnabar = (231, 71, 29)       #red
+forest_green = (34,139,34)     #light green
+japanese_laurel = (0,128,0)    #green
+camarone = (0,100,0)           #dark green
+dark_fern = (7, 72, 11)        #darker green
+malibu = (98, 164, 250)        #blue
+
+class SNAKE:
+	def __init__(self):
+		self.body = [Vector2(5,7), Vector2(4,7), Vector2(3,7)]
+		self.direction = Vector2(1, 0)
+		self.new_block = False
+
+	def draw_snake(self):
+		for block in self.body:
+			x_pos = int(block.x * cell_size)
+			y_pos = int(block.y * cell_size)
+			block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+			pygame.draw.rect(screen, malibu, block_rect)
+   
+	def move_snake(self):
+		if self.new_block == True:
+			body_copy = self.body[:]
+			body_copy.insert(0, body_copy[0] + self.direction)
+			self.body = body_copy[:]
+			self.new_block = False
+		else:
+			body_copy = self.body[:-1]
+			body_copy.insert(0, body_copy[0] + self.direction)
+			self.body = body_copy[:]
+
+	def add_block(self):
+		self.new_block = True
+
+class FRUIT:
+	def __init__(self):
+		self.randomize()
+
+	def draw_fruit(self):
+		fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+		pygame.draw.rect(screen, cinnabar, fruit_rect)
+
+	def randomize(self):
+		self.x = random.randint(0, cell_number - 1)
+		self.y = random.randint(0, cell_number - 1)
+		self.pos = Vector2(self.x, self.y)
+
+class MAIN:
+	def __init__(self):
+		self.snake = SNAKE()
+		self.fruit = FRUIT()
+
+	def update(self):
+		self.snake.move_snake()
+		self.check_collision()
+		self.check_fail()
+  
+	def draw_elements(self):
+		self.fruit.draw_fruit()
+		self.snake.draw_snake()
+		self.draw_score()
+
+	def check_collision(self):
+		if self.fruit.pos == self.snake.body[0]:
+			self.fruit.randomize()
+			self.snake.add_block()
+
+	def check_fail(self):
+		if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+			self.game_over()
+
+		for block in self.snake.body[1:]:
+			if block == self.snake.body[0]:
+				self.game_over()
+	def game_over(self):
+		pygame.quit()
+		sys.exit()
+
+	def draw_score(self):
+		pygame.draw.rect(screen, camarone, pygame.Rect(0, 0, 140, 35))
+		score_text = "Your Score: " + str(len(self.snake.body) - 3)
+		score_surface = game_font.render(score_text, True, black)
+		score_x = int(cell_size * cell_number - cell_size * cell_number * 0.9)
+		score_y = int(cell_size * cell_number - cell_size * cell_number * 0.97)
+		score_rect = score_surface.get_rect(center = (score_x, score_y))
+		screen.blit(score_surface,score_rect)
+     
 pygame.init()
- 
-black = (0, 0, 0)
-red = (139, 0, 0)
-green = (34,139,34)
-background_green = (0,128,0)
- 
-display_width = 600
-display_height = 400
- 
-dis = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('Maths Snake Game')
- 
+
+# Variables used for window size
+cell_size = 40
+cell_number = 15
+
+# Initialise game window
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
- 
-snake_block = 10
-snake_speed = 30
- 
-font_style = pygame.font.SysFont("arial", 25)
-score_font = pygame.font.SysFont("arial", 35)
- 
- 
-def Player_score(score):
-    pygame.draw.rect(dis, background_green, pygame.Rect(0, 0, 240, 40))
-    pygame.display.flip()
-    value = score_font.render("Your Score: " + str(score), True, black)
-    dis.blit(value, [0, 0])
- 
- 
- 
-def snake(snake_block, Snake_list):
-    for x in Snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
- 
- 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [display_width / 6, display_height / 3])
- 
- 
-def gameLoop():
-    game_over = False
-    game_close = False
- 
-    x1 = display_width / 2
-    y1 = display_height / 2
- 
-    x1_change = 0
-    y1_change = 0
- 
-    Snake_list = []
-    Length_of_snake = 1
- 
-    foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, display_height - snake_block) / 10.0) * 10.0
- 
-    while not game_over:
- 
-        while game_close == True:
-            dis.fill(green)
-            message("You Lost! Press R- to replay or Q-Quit", black)
-            Player_score(Length_of_snake - 1)
-            pygame.display.update()
- 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_r:
-                        gameLoop()
- 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
- 
-        if x1 >= display_width or x1 < 0 or y1 >= display_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(green)
-        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        Snake_list.append(snake_Head)
-        if len(Snake_list) > Length_of_snake:
-            del Snake_list[0]
- 
-        for x in Snake_list[:-1]:
-            if x == snake_Head:
-                game_close = True
- 
-        snake(snake_block, Snake_list)
-        Player_score(Length_of_snake - 1)
- 
-        pygame.display.update()
- 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, display_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
- 
-        clock.tick(snake_speed)
- 
-    pygame.quit()
-    quit()
- 
- 
-gameLoop()
+
+game_font = pygame.font.Font(None, 25)
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
+main_game = MAIN()
+
+# Main logic
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == SCREEN_UPDATE:
+			main_game.update()
+		# Whenever a key is pressed down
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				if main_game.snake.direction.y != 1: # Making sure the snake cannot move in the opposite direction instantaneously
+					main_game.snake.direction = Vector2(0, -1)
+			if event.key == pygame.K_RIGHT:
+				if main_game.snake.direction.x != -1: # Making sure the snake cannot move in the opposite direction instantaneously
+					main_game.snake.direction = Vector2(1, 0)
+			if event.key == pygame.K_DOWN:
+				if main_game.snake.direction.y != -1: # Making sure the snake cannot move in the opposite direction instantaneously
+					main_game.snake.direction = Vector2(0, 1)
+			if event.key == pygame.K_LEFT:
+				if main_game.snake.direction.x != 1: # Making sure the snake cannot move in the opposite direction instantaneously
+					main_game.snake.direction = Vector2(-1,0)
+			# Esc -> Create event to quit the game
+			elif event.key == pygame.K_ESCAPE:
+				pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+	screen.fill(japanese_laurel)
+	main_game.draw_elements()
+	pygame.display.update()
+	clock.tick(60)
